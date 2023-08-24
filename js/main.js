d3c9d7c441ce5b94eecc083fb4925c3bf9397eb7
Rodrigo Vazquez <rodrigo.e.vazquez@gmail.com>
2023-08-24 17:20:30 -0300

    second partial

--- js/main.js
+++ js/main.js
@@ -1,4 +1,53 @@
-let listado = [];
+let listado = [
+    { nombre: 'Empleado1', apellido: 'Apellido1', base: 'Base1' },
+    { nombre: 'Empleado2', apellido: 'Apellido2', base: 'Base2' },
+    { nombre: 'Empleado3', apellido: 'Apellido3', base: 'Base1' },
+    { nombre: 'Empleado4', apellido: 'Apellido4', base: 'Base2' },
+    { nombre: 'Empleado5', apellido: 'Apellido5', base: 'Base1' },
+    { nombre: 'Empleado6', apellido: 'Apellido6', base: 'Base2' },
+    { nombre: 'Empleado7', apellido: 'Apellido7', base: 'Base1' },
+    { nombre: 'Empleado8', apellido: 'Apellido8', base: 'Base2' },
+    { nombre: 'Empleado9', apellido: 'Apellido9', base: 'Base1' },
+    { nombre: 'Empleado10', apellido: 'Apellido10', base: 'Base2' },
+];
+
+// Función para asignar agente
+function asignarAgente() {
+    for (let i = 0; i < listado.length; i++) {
+        let fecha = new Date().toLocaleDateString();
+        let asistencia = prompt(`¿Está presente el empleado ${listado[i].nombre}? (Responda "si" o "no")`).toLowerCase();
+        let puntoAsignado = asistencia === 'no' ? 'AUSENTE' : prompt('Ingrese el punto asignado:');
+
+        listado[i].fecha = fecha;
+        listado[i].asistencia = asistencia === 'si' ? 'Presente' : 'Ausente';
+        listado[i].puntoAsignado = puntoAsignado;
+    }
+
+    // Actualizar lista
+    actualizarLista();
+}
+
+// Funcion para agregar nuevo empleado
+function agregarEmpleado() {
+    let nombre = prompt('Ingrese el nombre:');
+    let apellido = prompt('Ingrese el apellido:');
+    let baseInput = prompt('Ingrese la base (Elija entre "Base1" o "Base2"):').toLowerCase();
+    while (baseInput !== 'base1' && baseInput !== 'base2') {
+        baseInput = prompt('Base inválida. Ingrese la base (Elija entre "Base1" o "Base2"):').toLowerCase();
+    }
+
+    let fecha = new Date().toLocaleDateString();
+
+    listado.push({
+        nombre: nombre,
+        apellido: apellido,
+        base: baseInput,
+        fecha: fecha
+    });
+
+    actualizarLista();
+}
+
 //funcion calcular presentes y ausentes
 function calcularPresentesAusentes() {
     let presentes = 0;
@@ -16,32 +65,7 @@ function calcularPresentesAusentes() {
     console.log(`Cantidad de Presentes: ${presentes}`);
     console.log(`Cantidad de Ausentes: ${ausentes}`);
 }
-//funcion registrar asistencia
-function registrarAsistencia() {
-    //bucle while para repetir funcion
-    while (true){ 
-        let nombre = prompt('Ingrese el nombre, para salir presione 0:');
-        if (nombre === '0') {
-            break; // Salir del bucle si el usuario escribe "0"
-        }
-        let apellido = prompt('Ingrese el apellido:');
-        let asistencia = prompt('¿Está presente? (Responda "si" o "no"):').toLowerCase();
-        let puntoAsignado = asistencia === 'no' ? 'AUSENTE' : prompt('Ingrese el punto asignado:');
-    // Fecha actual
-    let fecha = new Date().toLocaleDateString();
-    //listado 
-    listado.push({
-        nombre: nombre,
-        apellido: apellido,
-        asistencia: asistencia === 'si' ? 'Presente' : 'Ausente',
-        fecha: fecha,
-        puntoAsignado: puntoAsignado,
-    });
 
-    // Actualizar lista
-    actualizarLista();
-    }
-}
 
 //funcion actualizar lista
 function actualizarLista() {
@@ -51,25 +75,56 @@ function actualizarLista() {
     }
 }
 
-    //cartel de opciones
-    let opcion;
+//funcion para filtrar por base con dos variables dentro, que son igual al listado mas la funcion filter con el parametro base
+function filtrarPorBase() {
+    const empleadosBase1 = listado.filter(empleado => empleado.base.toLowerCase() === 'base1');
+    const empleadosBase2 = listado.filter(empleado => empleado.base.toLowerCase() === 'base2');
+    return [empleadosBase1, empleadosBase2];
+}
 
-    while (true) {
-        opcion = prompt("Presione R para registrar, C para calcular o S para salir").toLowerCase();
-    
-        if (opcion === "r") {
-            registrarAsistencia();
-        } else if (opcion === "c") {
-            calcularPresentesAusentes();
-        } else if (opcion === "s") {
-            alert("¡Hasta luego!");
-            break; 
-        } else {
-            alert("Opción no válida");
-        }
+// funcion para buscar objeto por nombre dentro del array .find 
+function buscarEmpleadoPorNombre(nombreBuscado) {
+    const empleadoEncontrado = listado.find(empleado => empleado.nombre.toLowerCase() === nombreBuscado.toLowerCase());
+
+    if (empleadoEncontrado) {
+        console.log(`Empleado encontrado: ${empleadoEncontrado.nombre} ${empleadoEncontrado.apellido}`);
+    } else {
+        console.log(`No se encontró ningún empleado con el nombre "${nombreBuscado}"`);
     }
-    
+}
+
+
+// Menú de opciones
+let opcion;
+
+while (true) {
+    opcion = prompt("Presione A para asignar agente, N para agregar empleado, C para calcular, F para filtrar por base, B para buscar o S para salir").toLowerCase();
+
+    if (opcion === "a") {
+        asignarAgente();
+    } else if (opcion === "n") {
+        agregarEmpleado();
+    } else if (opcion === "c") {
+        calcularPresentesAusentes();
+    } else if (opcion === "f") {
+        //mostrar variables empleadosbase1 y empleadosbase2, filtradas segun su base con la funcion filtrarporbase 
+        const [empleadosBase1, empleadosBase2] = filtrarPorBase();
         
+        console.log("Empleados con Base1:", empleadosBase1);
+        console.log("Empleados con Base2:", empleadosBase2);
+    } else if (opcion === "b") {
+        const nombreBuscado = prompt('Ingrese el nombre del empleado a buscar:');
+        buscarEmpleadoPorNombre(nombreBuscado);
+    } else if (opcion === "s") {
+        alert("¡Hasta luego!");
+        break; 
+    } else {
+        alert("Opción no válida");
+    }
+}     
 
 calcularPresentesAusentes();
-registrarAsistencia();
+
+
+
+//buscarEmpleado(arr, filtro) { }
